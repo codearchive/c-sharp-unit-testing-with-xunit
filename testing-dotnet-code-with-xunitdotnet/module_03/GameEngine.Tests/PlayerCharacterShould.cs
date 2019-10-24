6b0fc802fc26e380683b09468f959eabf404c736
Pavel Patrusov <pavelp9415@gmail.com>
2019-10-24 23:38:20 +0300

    Testing .NET Code with xUnit.net: Getting Started - 3.6 - Determining Passing and Failing Tests with Asserts: Asserting on Numeric Values

--- testing-dotnet-code-with-xunitdotnet/module_03/GameEngine.Tests/PlayerCharacterShould.cs
+++ testing-dotnet-code-with-xunitdotnet/module_03/GameEngine.Tests/PlayerCharacterShould.cs
@@ -79,5 +79,33 @@ namespace GameEngine.Tests
 
             Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
         }
+
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Equal(100, sut.Health);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth_NotEqualExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.NotEqual(0, sut.Health);
+        }
+
+
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.Sleep();
+
+            //Assert.True(sut.Health >= 100 && sut.Health <= 200);
+            Assert.InRange(sut.Health, 101, 200);
+        }
     }
 }
