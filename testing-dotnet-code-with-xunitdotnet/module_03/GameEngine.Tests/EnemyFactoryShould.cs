aca148a3595bae32ecff9b62ef68629c174e236e
Pavel Patrusov <pavelp9415@gmail.com>
2019-10-25 23:34:48 +0300

    Testing .NET Code with xUnit.net: Getting Started - 3.10 - Determining Passing and Failing Tests with Asserts: Making Asserts Against Object Types

---
+++ testing-dotnet-code-with-xunitdotnet/module_03/GameEngine.Tests/EnemyFactoryShould.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class EnemyFactoryShould
+    {
+        [Fact]
+        public void CreateNormalEnemyByDefault()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsType<NormalEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateNormalEnemyByDefault_NotTypeExample()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsNotType<DateTime>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsNotType<DateTime>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_CastReturnedTypeExample()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            BossEnemy boss = Assert.IsType<BossEnemy>(enemy);
+
+            Assert.Equal("Zombie King", boss.Name);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_AssertAssignableTypes()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsAssignableFrom<Enemy>(enemy);
+        }
+    }
+}
