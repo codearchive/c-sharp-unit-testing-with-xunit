ee5c324617e13756cfc272dbc790177ba5352a41
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-01 23:37:27 +0200

    Testing .NET Code with xUnit.net: Getting Started - 5.3 - Creating Data-driven Tests: Adding Extra PlayerCharacter Tests

---
+++ testing-dotnet-code-with-xunitdotnet/module_05/GameEngine.Tests/GameStateCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    [CollectionDefinition("GameState collection")]
+    public class GameStateCollection : ICollectionFixture<GameStateFixture>
+    {
+    }
+}
---
+++ testing-dotnet-code-with-xunitdotnet/module_05/GameEngine.Tests/PlayerCharacterShould.cs
@@ -0,0 +1,198 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    public class PlayerCharacterShould : IDisposable
+    {
+        private readonly PlayerCharacter _sut;
+        private readonly ITestOutputHelper _output;
+
+        public PlayerCharacterShould(ITestOutputHelper output)
+        {
+            _output = output;
+
+            _output.WriteLine("Creating new PlayerCharacter");
+            _sut = new PlayerCharacter();
+        }
+
+        public void Dispose()
+        {
+            _output.WriteLine($"[Application example:] Disposing PlayerCharacter {_sut.FullName}");
+
+            //_sut.Dispose();
+        }
+
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            Assert.True(_sut.IsNoob);
+        }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            _sut.FirstName = "Pavel";
+            _sut.LastName = "Patrusov";
+
+            Assert.Equal("Pavel Patrusov", _sut.FullName);
+            Assert.StartsWith("Pavel", _sut.FirstName);
+        }
+
+        [Fact]
+        public void HaveFullNameStartsWithFirstName()
+        {
+            _sut.FirstName = "Pavel";
+            _sut.LastName = "Patrusov";
+
+            Assert.StartsWith("Pavel", _sut.FirstName);
+        }
+
+        [Fact]
+        public void HaveFullNameEndingWithLastName()
+        {
+            _sut.FirstName = "Pavel";
+            _sut.LastName = "Patrusov";
+
+            Assert.StartsWith("Patrusov", _sut.LastName);
+        }
+
+        [Fact]
+        public void CalculateFullName_IgnoreCaseAssertExample()
+        {
+            _sut.FirstName = "PAVEL";
+            _sut.LastName = "PATRUSOV";
+
+            Assert.Equal("Pavel Patrusov", _sut.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void CalculateFullName_SubstringAssertExample()
+        {
+            _sut.FirstName = "Pavel";
+            _sut.LastName = "Patrusov";
+
+            Assert.Contains("el Pa", _sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullNameWithTitleCase()
+        {
+            _sut.FirstName = "Pavel";
+            _sut.LastName = "Patrusov";
+
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", _sut.FullName);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            Assert.Equal(100, _sut.Health);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth_NotEqualExample()
+        {
+            Assert.NotEqual(0, _sut.Health);
+        }
+
+
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            _sut.Sleep();
+
+            //Assert.True(sut.Health >= 100 && sut.Health <= 200);
+            Assert.InRange(_sut.Health, 101, 200);
+        }
+
+        [Fact]
+        public void NotHaveNickNameByDefault()
+        {
+            Assert.Null(_sut.Nickname);
+        }
+
+        [Fact]
+        public void HaveLongBow()
+        {
+            Assert.Contains("Long Bow", _sut.Weapons);
+        }
+
+
+        [Fact]
+        public void NotHaveAStaffOfWonder()
+        {
+            Assert.DoesNotContain("Staff of Wonder", _sut.Weapons);
+        }
+
+        [Fact]
+        public void HaveAtLeastOneKindOfSword()
+        {
+            Assert.Contains(_sut.Weapons, weapon => weapon.Contains("Sword"));
+        }
+
+        [Fact]
+        public void HaveAllExpectedWeapons()
+        {
+            var expectedWeapons = new[]
+            {
+                "Long Bow",
+                "Short Bow",
+                "Short Sword"
+            };
+
+            Assert.Equal(expectedWeapons,_sut.Weapons);
+        }
+
+        [Fact]
+        public void NotHaveEmptyDefaultWeapons()
+        {
+            Assert.All(_sut.Weapons, weapon => Assert.False(string.IsNullOrWhiteSpace(weapon)));
+        }
+
+        [Fact]
+        public void RaiseSleptEvent()
+        {
+            Assert.Raises<EventArgs>(handler => _sut.PlayerSlept += handler, handler => _sut.PlayerSlept -= handler, () => _sut.Sleep());
+        }
+
+        [Fact]
+        public void RaisePropertyChangedEvent()
+        {
+            Assert.PropertyChanged(_sut, "Health", () => _sut.TakeDamage(10));
+        }
+
+        [Fact]
+        public void TakeZeroDamage()
+        {
+            _sut.TakeDamage(0);
+
+            Assert.Equal(100, _sut.Health);
+        }
+
+        [Fact]
+        public void TakeSmallDamage()
+        {
+            _sut.TakeDamage(1);
+
+            Assert.Equal(99, _sut.Health);
+        }
+
+        [Fact]
+        public void TakeMediumDamage()
+        {
+            _sut.TakeDamage(50);
+
+            Assert.Equal(50, _sut.Health);
+        }
+
+        [Fact]
+        public void HaveMinimumHealth()
+        {
+            _sut.TakeDamage(101);
+
+            Assert.Equal(1, _sut.Health);
+        }
+    }
+}
